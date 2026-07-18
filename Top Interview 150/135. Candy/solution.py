13ae2c3186682bd2239c737d2d29593a24dcdcdd
mmdend <mmdend.dev@gmail.com>
2026-07-18 17:50:50 +0330

    solved: 135. Candy -> 8ms exe

---
+++ Top Interview 150/135. Candy/solution.py
@@ -0,0 +1,28 @@
+case1 = ([1, 0, 2],)
+case2 = ([1, 2, 2],)
+
+
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n
+        # for i in range(n - 1):
+        #     if ratings[i] > ratings[i + 1]:
+        #         candies[i] += 1
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        return sum(candies)
+
+
+s = Solution()
+print(s.candy(*case1))
+print(s.candy(*case2))
